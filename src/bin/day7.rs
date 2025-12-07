a5b3b90115d2a4571bc4ab5343ea2d82f6474f30
loudsheep <loudsheep05@gmail.com>
2025-12-07 10:43:31 +0100

    feat: day7 part2

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -6,7 +6,7 @@ fn is_line_empty(line: &String) -> bool {
     line.chars().all(|c| c == '.')
 }
 
-fn count_splits(lines: Vec<String>) -> usize {
+fn count_splits(lines: &Vec<String>) -> usize {
     let mut count = 0;
     let mut beams = HashSet::new();
 
@@ -33,9 +33,38 @@ fn count_splits(lines: Vec<String>) -> usize {
     count
 }
 
+fn count_timelines(lines: &Vec<String>) -> usize {
+    let mut timelines = vec![0; lines[0].len()];
+    
+    for line in lines {
+        if is_line_empty(&line) {
+            continue;
+        }
+
+        for (i, c) in line.chars().enumerate() {
+            if c == '.' {
+                continue;
+            }
+            if c == '^' && timelines[i] > 0 {
+                timelines[i + 1] += timelines[i];
+                timelines[i - 1] += timelines[i];
+
+                timelines[i] = 0;
+            } else if c == 'S' {
+                timelines[i] = 1;
+            }
+        }
+    }
+
+    timelines.iter().sum()
+}
+
 fn main() {
     let lines = read_lines("input/day7.txt").expect("EXpected file");
     
-    let result = count_splits(lines);
-    println!("Result: {}", result);
+    let result = count_splits(&lines);
+    println!("Splits: {}", result);
+
+    let result2 = count_timelines(&lines);
+    println!("Timelines: {}", result2);
 }
